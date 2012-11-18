d29d1d997f9cf8057c6cce86d4e0aef603dbb20c
Guz Alexander <kalimatas@gmail.com>
2012-11-18 14:33:56 +0400

    exclusive group

--- caesar.py
+++ caesar.py
@@ -8,8 +8,8 @@ MAX_SHIFT_SIZE = 26
 MODE_ENCRYPT='e'
 MODE_DECRYPT='d'
 
-def translate(input, key, mode=MODE_DECRYPT):
-    """Encrypt/dectypt input"""
+def translate(input, key, mode=MODE_ENCRYPT):
+    """Encrypt/decrypt input"""
     if key < 1 or key > 26:
         raise ValueError("key must beetween 1 and %s " % (str(MAX_SHIFT_SIZE)))
 
@@ -24,15 +24,21 @@ def translate(input, key, mode=MODE_DECRYPT):
 
 def getParser():
     """Configure params"""
-    parser = argparse.ArgumentParser(description='Encrypt/dectypt text with Caesar cipher')
+    parser = argparse.ArgumentParser(description='Encrypt/decrypt text with Caesar cipher')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-e', '--encrypt', action='store_true')
+    group.add_argument('-d', '--decrypt', action='store_true')
+
     parser.add_argument('-k', '--key', help='key length [1..' + str(MAX_SHIFT_SIZE) + ']', required=True, type=int)
-    parser.add_argument('-i', '--input', help='data to encrypt/decrypt', required=True)
+    parser.add_argument('data', help='data to encrypt/decrypt')
     return parser
 
 if __name__ == "__main__":
     parser = getParser()
     args = parser.parse_args()
     try:
-        print(translate(args.input, args.key))
+        mode = MODE_DECRYPT if args.decrypt else MODE_ENCRYPT
+        print(translate(args.data, args.key, mode))
     except Exception as e:
         print("Error: %s" % (e))
